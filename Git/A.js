f5860f3da793c0cdb3f868847b4e65c00622ea18
Nizamuddin08 <100946134+Nizamuddin08@users.noreply.github.com>
2022-03-04 19:19:09 +0530

    Add files via upload

---
+++ Git/A.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function A() {
+  return (
+    <div>
+      <div>Hello mifify from master</div>
+
+      <div>Hello modified in br1</div>
+    </div>
+  );
+}
+
+export default A;
---
+++ Git/B.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function B() {
+  return <div>World</div>;
+}
+
+export default B;
